ab18950590ddc5c66ab259d5abd490c7540756d1
dmosher42@gmail.com <dmosher42@gmail.com>
2015-04-15 15:22:30 -1000

    Switched to using svg.path for parsing SVG files.
    SVG doesn't store points as coords. Using svg.path to parse. Could even do it for curves...
    
    HOW TO GET svg.path ON MAC:
    1. open terminal
    2. type "sudo easy_install svg.path
    3. type in password
    That should do it.

--- Main.py
+++ Main.py
@@ -11,8 +11,13 @@ https://docs.python.org/2/tutorial/modules.html
 
 filelocation = 'test.svg'
 
+from xml.etree import ElementTree
+
+
 import xml.etree.ElementTree as ET
-import sys
+#import sys
+from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
+
 #import xml.etree.ElementTree as ET2
 #import string as ST
 tree = ET.parse(filelocation)
@@ -22,12 +27,29 @@ ns = {'svg':'http://www.w3.org/2000/svg','is':'http://www.inkscape.org/namespace
 #tree2 = ET.parse('test2.fab')
 
 convunit = .264583
-xoffset = 100
-yoffset = 10
+xoffset = 80
+yoffset = 50
 pointz = 0.390000
-dplaces = '%.6f'
+dplaces = '%.6f'  #number of decimal places. dplaces % number
 
-print(sys.version)
+
+def indent(elem, level=0):
+    i = "\n" + level*"   "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "   "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+
+#print(sys.version)
 
 paths = []
 points = []
@@ -38,13 +60,31 @@ for child in root:
 
 print("=======GET SVG PATHS:======")
 
+points=[]
+
 for path in root.findall('svg:path',ns):
+    print(path.get('d'))
+    parpath = parse_path(path.get('d'))
+    print(len(parpath))
+    for i in range(0,len(parpath)):
+        pt = []
+        pt.append(parpath[i].point(0).real)
+        pt.append(parpath[i].point(0).imag)
+        points.append(pt)
+        print(pt)
+    paths.append(points)
+
+
+'''
     points=(str.split(path.get('d')))
     points.pop(0) #remove the M/m
     for i in range(len(points)):
         points[i] = str.split(points[i],",")
+        if "z" in points[i]:
+            points.pop()
     paths.append(points)
     #points.append()
+'''
 
 print("=======GET SVG RECTS:======")
 #not really needed right now
@@ -64,6 +104,7 @@ print(points)
 
 builder=ET.TreeBuilder()
 fabhpr = ET.fromstring("<fabAtHomePrinter><printAcceleration>100</printAcceleration><materialCalibration><clearance>2</clearance><depositionRate>0.005</depositionRate><name>epoxy</name><pathSpeed>5</pathSpeed><pathWidth>1</pathWidth><pausePaths>30</pausePaths><pitch>0.000397</pitch><pushout>0.3</pushout><suckback>0.25</suckback><suckbackDelay>0</suckbackDelay></materialCalibration><materialCalibration><clearance>1</clearance><depositionRate>0.00085</depositionRate><name>silicone</name><pathSpeed>9</pathSpeed><pathWidth>0.4</pathWidth><pausePaths>300</pausePaths><pitch>0.000397</pitch><pushout>0.11</pushout><suckback>0.11</suckback><suckbackDelay>0</suckbackDelay></materialCalibration></fabAtHomePrinter>")
+indent(fabhpr)
 
 for j in range(len(paths)):
     print(paths[j])
@@ -99,6 +140,8 @@ for j in range(len(paths)):
         pointr.insert(2,zr)
         pathr.insert(j+i+1,pointr)
     fabhpr.insert(j+3,pathr)
+    indent(pathr)
+
 
 
 print("\n\nDEBUGTIME")
@@ -108,10 +151,13 @@ for der in fabhpr:
 print ET.tostring(fabhpr)
 
 print(pathr.tag)
-print ET.tostring(pathr)
+indent(pathr)
 
+print ET.tostring(pathr)
 fulltree=ET.ElementTree(fabhpr)
 #The tree is initialized with the contents of the XML file if given.
 
-fulltree.write("testout.xml", "us-ascii", None, None, method="xml")
 
+fulltree.write("page.fab",
+           xml_declaration=True,encoding='utf-8',
+           method="xml")
